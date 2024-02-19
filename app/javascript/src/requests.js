f43e0cad4cd2f7ffcc69e8c82ee03824859967c6
Joanna092 <kochanowska92@gmail.com>
2024-02-20 08:56:17 +0900

    created postTask function

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,1 +1,44 @@
-console.log('ajax requests');
+import $ from 'jquery';
+
+var indexTasks = function () {
+  var request = {
+    type: 'GET',
+    url: 'api/tasks?api_key=1',
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (request, errorMsg) {
+      console.log(request, errorMsg);
+    }
+  }
+
+  $.ajax(request);
+};
+
+indexTasks();
+
+var postTask = function (content) {
+    var request = {
+      type: 'POST',
+      url: 'api/tasks?api_key=1',
+      // Add CSRF token in headers
+      headers: {
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+      },
+      data: {
+        task: {
+          content: content
+        }
+      },
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (request, errorMsg) {
+        console.log(request, errorMsg);
+      }
+    }
+  
+    $.ajax(request);
+  };
+  
+  
