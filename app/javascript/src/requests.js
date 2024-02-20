ca864b3a7293b60705e34c51965e2a6c7757c767
Joanna092 <kochanowska92@gmail.com>
2024-02-20 09:01:06 +0900

    moved CRSF token to the top of the page

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
 var indexTasks = function () {
   var request = {
     type: 'GET',
@@ -22,9 +28,6 @@ var postTask = function (content) {
       type: 'POST',
       url: 'api/tasks?api_key=1',
       // Add CSRF token in headers
-      headers: {
-        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-      },
       data: {
         task: {
           content: content
